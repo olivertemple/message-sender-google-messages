c9a5b154291c3e960da1f234d668512616d775b8
olivertemple <70803502+olivertemple@users.noreply.github.com>
2021-01-12 10:48:56 +0000

    fully operational google messages for web sender

---
+++ main.py
@@ -0,0 +1,40 @@
+import pyautogui as pa
+import time
+
+file = open("text.txt",'r')
+mylist = file.read()
+#print(mylist)
+newlist = []
+
+mylist=mylist.split('.')
+for item in mylist:
+    newlist.append(item.replace('\n',''))
+
+#pa.moveTo(100,100, duration=1)
+#pa.click(x=None,y=None, interval=0.0, button='left',duration=0.0)
+pa.press('win')
+pa.typewrite("chrome")
+pa.press('enter')
+time.sleep(1)
+pa.typewrite("https://messages.google.com/web")
+pa.press('enter')
+pa.keyDown('win')
+pa.press('up')
+pa.keyUp('win')
+time.sleep(4)
+pa.keyDown("ctrl")
+pa.keyDown("alt")
+pa.press("c")
+pa.keyUp('ctrl')
+pa.keyUp('alt')
+pa.typewrite(newlist[0])
+time.sleep(3)
+pa.press('enter')
+time.sleep(1)
+for line in range(1,len(newlist)):
+    if newlist[line]!='':
+        pa.typewrite(newlist[line])
+        pa.press('enter')
+
+
+        
